d6427d14e8f0d41406741d00fba0a9a609fe695b
vanessa <vanessa@10.245.44.25>
2018-04-11 16:31:29 -0400

    Task 1 works as expected

--- src/Skyscraper.java
+++ src/Skyscraper.java
@@ -7,40 +7,54 @@ public class Skyscraper {
     public static int c;
 
     public static void main(String[] args) {
-        // readInVariables();
+        Scanner input = new Scanner(System.in);
 
-        n = 100; // TODO: FOR TESTING ONLY
-        m = 100; // TODO: FOR TESTING ONLY
-        c = 1; // TODO: FOR TESTING ONLY
+        readInVariables(input);
 
         int x = Integer.parseInt(args[0]);
         switch (x) {
         case 1:
-            firstTask();
+            firstTask(input);
             break;
         case 2:
-            secondTask();
+            secondTask(input);
             break;
         case 3:
-            thirdTask();
+            thirdTask(input);
             break;
         case 4:
-            fourthTask();
+            fourthTask(input);
             break;
         }
+
+        input.close();
     }
 
-    public static void readInVariables() {
+    public static void readInVariables(Scanner input) {
         System.out.println("Enter your variables N, M, and C, separated by one space: ");
 
-        Scanner input = new Scanner(System.in);
         n = input.nextInt();
         m = input.nextInt();
         c = input.nextInt();
 
         System.out.printf("N = %d, M = %d, C = %d.\n", n, m, c);
+    }
 
-        input.close();
+    /**
+     * Returns the minimum of three ints
+     * 
+     * @param a
+     *            buffer[j], the int above the current cell
+     * @param b
+     *            buffer[j - 1], the int above-left of the current cell
+     * @param c
+     *            tmpBuf[j - 1], the int left of the current cell
+     * @return the minimum of the three cell values
+     */
+    public static int getMin(int a, int b, int c) {
+        int min = Math.min(a, b);
+        min = Math.min(min, c);
+        return min;
     }
 
     /**
@@ -54,35 +68,57 @@ public class Skyscraper {
      * line of length M, and read the next line as it comes through the input
      * stream. This ensures we find our solution in Θ(N*M) time and with O(M) space.
      */
-    public static void firstTask() {
+    public static void firstTask(Scanner stream) {
         System.out.println("You are starting the first task.");
 
+        // We are using two arrays of length m, which makes this algorithm operate with
+        // O(2M) space, which means it is in O(M) space
         int[] buffer = new int[m];
+        int[] tmpBuf = new int[m];
 
-        Scanner stream = new Scanner(System.in);
-        
         int[] maxSquare = new int[2];
         maxSquare[0] = -1;
         maxSquare[1] = -1;
+
+        int currentMax = -1;
 
         /* This for-loop iterates n*m times, which makes this algorithm work in Θ(N*M)
          * time */
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (stream.nextInt() == c) {
-                   // buffer[m] = Math.min(a, b);
+                int current = stream.nextInt();
+                if (current == c) {
+                    if (i == 0 || j == 0) {
+                        tmpBuf[j] = 1;
+                    } else {
+                        int min = getMin(buffer[j], buffer[j - 1], tmpBuf[j - 1]);
+                        tmpBuf[j] = min + 1;
+                    }
+                } else {
+                    tmpBuf[j] = 0;
+                }
+
+                if (tmpBuf[j] > currentMax) {
+                    currentMax = tmpBuf[j];
+                    maxSquare[0] = j;
+                    maxSquare[1] = i;
                 }
             }
+            buffer = tmpBuf;
+            tmpBuf = new int[m];
         }
 
-        stream.close();
+        int dif = currentMax - 1;
+        // As per instructions in the class online discussion section, assume x is for
+        // rows, and y is for columns
+        System.out.printf("%d %d %d %d\n", maxSquare[1] - dif + 1, maxSquare[0] - dif + 1, maxSquare[1] + 1, maxSquare[0] + 1);
     }
 
     /**
      * This is a Θ(N*M^2) time algorithm that uses O(M) space for computing a
      * largest area rectangle block where all cells have height C.
      */
-    public static void secondTask() {
+    public static void secondTask(Scanner stream) {
         System.out.println("You are starting the second task.");
     }
 
@@ -90,7 +126,7 @@ public class Skyscraper {
      * This is an Θ(N*M) time algorithm that uses O(M) space for computing a largest
      * area rectangle block where all cells have height C.
      */
-    public static void thirdTask() {
+    public static void thirdTask(Scanner stream) {
         System.out.println("You are starting the third task.");
     }
 
@@ -99,7 +135,7 @@ public class Skyscraper {
      * largest area rectangle block with the height difference of at most C for any
      * two cells in the block.
      */
-    public static void fourthTask() {
+    public static void fourthTask(Scanner stream) {
         System.out.println("You are starting the fourth task.");
     }
 
