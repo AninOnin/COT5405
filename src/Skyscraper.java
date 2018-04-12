aec74e98c8fc6b62c7c789d6b8f564fd9f6782c7
vfrost <vfrost@192.168.0.7>
2018-04-11 21:44:37 -0400

    working on task 2

--- src/Skyscraper.java
+++ src/Skyscraper.java
@@ -83,8 +83,10 @@ public class Skyscraper {
 
         int currentMax = -1;
 
-        /* This for-loop iterates n*m times, which makes this algorithm work in Θ(N*M)
-         * time */
+        /*
+         * This for-loop iterates n*m times, which makes this algorithm work in Θ(N*M)
+         * time
+         */
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 int current = stream.nextInt();
@@ -112,7 +114,8 @@ public class Skyscraper {
         int dif = currentMax - 1;
         // As per instructions in the class online discussion section, assume x is for
         // rows, and y is for columns
-        System.out.printf("%d %d %d %d\n", maxSquare[1] - dif + 1, maxSquare[0] - dif + 1, maxSquare[1] + 1, maxSquare[0] + 1);
+        System.out.printf("%d %d %d %d\n", maxSquare[1] - dif + 1, maxSquare[0] - dif + 1, maxSquare[1] + 1,
+                maxSquare[0] + 1);
     }
 
     /**
@@ -139,26 +142,28 @@ public class Skyscraper {
         maxSquare[1] = -1;
 
         int currentMax = -1;
+        int maxArea = -1;
 
-        /* This for-loop iterates n*m times, which makes this algorithm work in Θ(N*M)
-         * time */
+        /*
+         * This for-loop iterates n*m times, which makes this algorithm work in Θ(N*M)
+         * time
+         */
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 int current = stream.nextInt();
                 if (current == c) {
-                    if (i == 0) {
+                    if (i != 0) {
+                        buffer[j]++;
+                    } else {
                         buffer[j] = 1;
                     }
                 } else {
                     buffer[j] = 0;
                 }
 
-                getMaxArea(buffer);
-
-                if (tmpBuf[j] > currentMax) {
-                    currentMax = tmpBuf[j];
-                    maxSquare[0] = j;
-                    maxSquare[1] = i;
+                int temp = getMaxArea(buffer);
+                if (maxArea < temp) {
+                    maxArea = temp;
                 }
             }
         }
@@ -166,49 +171,38 @@ public class Skyscraper {
         int dif = currentMax - 1;
         // As per instructions in the class online discussion section, assume x is for
         // rows, and y is for columns
-        System.out.printf("%d %d %d %d\n", maxSquare[1] - dif + 1, maxSquare[0] - dif + 1, maxSquare[1] + 1, maxSquare[0] + 1);
+        System.out.printf("%d %d %d %d\n", maxSquare[1] - dif + 1, maxSquare[0] - dif + 1, maxSquare[1] + 1,
+                maxSquare[0] + 1);
     }
 
     public static int getMaxArea(int[] buffer) {
-        Deque<Integer> stack = new LinkedList<Integer>();
-        int maxArea = 0;
-        int area = 0;
+        Deque<Integer> height = new LinkedList<Integer>();
+
+        int maxArea = -1;
+        int area = -1;
         int i;
+
         for (i = 0; i < buffer.length;) {
-            if (stack.isEmpty() || buffer[stack.peekFirst()] <= buffer[i]) {
-                stack.offerFirst(i++);
+            if (height.isEmpty() || buffer[height.peekFirst()] <= buffer[i]) {
+                height.offerFirst(i++);
             } else {
-                int top = stack.pollFirst();
-                // if stack is empty means everything till i has to be
-                // greater or equal to input[top] so get area by
-                // input[top] * i;
-                if (stack.isEmpty()) {
+                int top = height.pollFirst();
+                if (height.isEmpty()) {
                     area = buffer[top] * i;
-                }
-                // if stack is not empty then everythin from i-1 to input.peek() + 1
-                // has to be greater or equal to input[top]
-                // so area = input[top]*(i - stack.peek() - 1);
-                else {
-                    area = buffer[top] * (i - stack.peekFirst() - 1);
+                } else {
+                    area = buffer[top] * (i - height.peekFirst() - 1);
                 }
                 if (area > maxArea) {
                     maxArea = area;
                 }
             }
         }
-        while (!stack.isEmpty()) {
-            int top = stack.pollFirst();
-            // if stack is empty means everything till i has to be
-            // greater or equal to input[top] so get area by
-            // input[top] * i;
-            if (stack.isEmpty()) {
+        while (!height.isEmpty()) {
+            int top = height.pollFirst();
+            if (height.isEmpty()) {
                 area = buffer[top] * i;
-            }
-            // if stack is not empty then everything from i-1 to input.peek() + 1
-            // has to be greater or equal to input[top]
-            // so area = input[top]*(i - stack.peek() - 1);
-            else {
-                area = buffer[top] * (i - stack.peekFirst() - 1);
+            } else {
+                area = buffer[top] * (i - height.peekFirst() - 1);
             }
             if (area > maxArea) {
                 maxArea = area;
